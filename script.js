f26a7d885c613450b68a991eb6d3883ed7dd4fad
Iamdjaoued <89752580+Iamdjaoued@users.noreply.github.com>
2021-08-29 23:50:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function redpillfunction() {
+document.getElementById("Title_Result").innerHTML = null
+document.getElementById("Title_Result").innerHTML = "good job you smart !!";
+}
+
+function bluepillfunction() {
+    document.getElementById("Title_Result").innerHTML = null
+    document.getElementById("Title_Result").innerHTML = "wlh yla hmar a sidi pff";
+}
